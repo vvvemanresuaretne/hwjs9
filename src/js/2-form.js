2ce15add599e090bb2f358be42439cd9dee44b00
vvvemanresuaretne <vvvasilkovskyi@gmail.com>
2025-04-14 00:00:11 +0200

    Initial commit

---
+++ src/js/2-form.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = { email: '', message: '' };
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  formData = JSON.parse(savedData);
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (!formData.email.trim() || !formData.message.trim()) {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: '', message: '' };
+  form.reset();
+});
